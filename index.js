211482dbaf233ab1c6fddcb45a439ba58cd9d8d1
jaykit <jaykitmaurya19@gmail.com>
2024-12-08 21:47:04 +0530

    added

---
+++ index.js
@@ -0,0 +1,78 @@
+const express=require("express");
+const nodeMailer=require("nodemailer");
+
+const cors=require("cors")
+require("dotenv").config();
+
+const app=express();
+const coroption={
+    origin:"http://localhost:3000",
+    methods:["GET","POST","PUT","DELETE"],
+    credentials:true,
+
+}
+
+
+
+const transport= nodeMailer.createTransport({
+    host:"smtp.gmail.com",
+    auth:{
+        user:"jaykitmaurya1907@gmail.com",
+        pass:process.env.MAIL_PASS
+    }
+})
+
+var mailoption={ }
+function mailfun(to,sub,msg){
+         mailoption={
+        from:"jaykitmaurya1907@gmail.com",
+        to:to,
+        subject:sub,
+        text:msg
+        
+    }
+}
+    
+    
+app.use(express.json());
+app.use(cors(coroption));
+
+app.get("/",(req,res)=>{
+    res.json("home page");
+});
+
+app.post("/sendmail",(req,res)=>{
+
+
+     mailfun( req.body.to , req.body.subject, req.body.message);
+     console.log(req.body.to +req.body.subject+ req.body.message)
+
+        transport.sendMail(mailoption,(err,data)=>{
+        if(err){
+            console.log("sending error");
+            console.log("error:"+err);
+        }
+        else{
+           
+           res.send(`
+            <html>
+            <body>
+            <script>
+            alert("send successsfully..");
+            </script>
+            </body>
+            </html>
+            `);
+            console.log("successfuly send mail");
+        }
+    })
+
+  
+    
+});
+
+
+port=process.env.PORT || 4000;
+app.listen(port,()=>{
+    console.log(`server running on ${port}`);
+});
